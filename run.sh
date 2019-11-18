dce4911b9d5d6d96c7880c6e11b23e50c2dd4d06
Frankie <zaihuazhao@163.com>
2019-11-18 20:07:14 +0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+export PYENV_ROOT="/root/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
 
-cd /root/beep_crawler 
+cd /root/beep_crawler
 
 # pyenv 不能在shell中激活【问题】
-# pyenv activate beep_crawler && 
-
-scrapy crawl bishijie  -s JOBDIR=jobs/bishijie-1
-scrapy crawl jinse  -s JOBDIR=jobs/jinse-1
+pyenv activate beep_crawler && scrapy crawl bishijie  -s JOBDIR=jobs/bishijie-1
+pyenv activate beep_crawler && scrapy crawl jinse  -s JOBDIR=jobs/jinse-1
 
 # 币世界
-
-
-
