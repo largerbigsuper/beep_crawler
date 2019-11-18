2068f03f36166e7949a537247f171fabc652d70f
Frankie <zaihuazhao@163.com>
2019-11-18 20:21:08 +0800

    add odaily

---
+++ beep_crawler/spiders/odaily.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import hashlib
+import json
+from datetime import datetime
+
+import scrapy
+
+from beep_crawler.items import BeepCrawlerItem
+
+
+class OdailySpider(scrapy.Spider):
+    name = 'odaily'
+    allowed_domains = ['odaily.com']
+    start_urls = ['https://www.odaily.com/api/pp/api/info-flow/newsflash_columns/newsflashes?b_id=&per_page=20']
+
+    def parse(self, response):
+        site_name = '星球日报'
+        crawled_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        news_list = []
+        ret_json = json.loads(response.body)
+        news_list = ret_json['data']['items']
+        for news in news_list:
+            item = BeepCrawlerItem()
+            item['title'] = news['title']
+            item['content'] = news['description']
+            item['source'] = ''
+            item['link'] = news['news_url']
+            item['published_at'] = news['published_at']
+            item['crawled_at'] = crawled_at
+            item['site_name'] = site_name
+            item['md5_content'] = hashlib.md5(item['content'].encode('utf8')).hexdigest()
+            # print(item)
+            yield item
+
--- run.sh
+++ run.sh
@@ -8,5 +8,6 @@ cd /root/beep_crawler
 # pyenv 不能在shell中激活【问题】
 pyenv activate beep_crawler && scrapy crawl bishijie  -s JOBDIR=jobs/bishijie-1
 pyenv activate beep_crawler && scrapy crawl jinse  -s JOBDIR=jobs/jinse-1
+pyenv activate beep_crawler && scrapy crawl odaily  -s JOBDIR=jobs/odaily-1
 
 # 币世界
