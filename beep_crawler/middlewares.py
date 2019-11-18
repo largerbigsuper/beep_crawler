ee609ed3956329cc214a52a2f92dcb7734a6c2ff
Frankie <zaihuazhao@163.com>
2019-11-18 22:45:41 +0800

    fix user-agent middleware

--- beep_crawler/middlewares.py
+++ beep_crawler/middlewares.py
@@ -152,4 +152,4 @@ class BeepCrawlerHeaderMiddleware(object):
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
         ]
         agent = random.choice(user_agent_list)
-        request.headers['User_Agent'] = agent
+        request.headers['User-Agent'] = agent
