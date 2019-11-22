48e2a4d3ebc2ccf6dd77b9e41efd4b53c470d64b
Frankie <zaihuazhao@163.com>
2019-11-23 01:17:05 +0800

    rm 深链

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ cd /root/beep_crawler
 pyenv activate beep_crawler && scrapy crawl bishijie  -s JOBDIR=jobs/bishijie-1
 pyenv activate beep_crawler && scrapy crawl jinse  -s JOBDIR=jobs/jinse-1
 pyenv activate beep_crawler && scrapy crawl odaily  -s JOBDIR=jobs/odaily-1
-pyenv activate beep_crawler && scrapy crawl shenliancaijing  -s JOBDIR=jobs/shenliancaijing-1
+# pyenv activate beep_crawler && scrapy crawl shenliancaijing  -s JOBDIR=jobs/shenliancaijing-1
 pyenv activate beep_crawler && scrapy crawl chainnews  -s JOBDIR=jobs/chainnews-1
 
 # 币世界
